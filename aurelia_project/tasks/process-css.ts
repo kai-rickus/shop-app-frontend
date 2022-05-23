c8f8760cbdc09b222fb92116203cf1dc65683305
kai-rickus <kai.rickus@outlook.com>
2022-05-23 15:14:32 +0200

    update build CSS

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -1,4 +1,4 @@
-import {CLIOptions, build} from 'aurelia-cli';
+import { CLIOptions, build } from 'aurelia-cli';
 import * as gulp from 'gulp';
 import * as project from '../aurelia.json';
 import * as sass from 'gulp-dart-sass';
@@ -8,21 +8,28 @@ import * as autoprefixer from 'autoprefixer';
 import * as cssnano from 'cssnano';
 import * as postcssUrl from 'postcss-url';
 
-export default function processCSS() {
-  return gulp.src(project.cssProcessor.source, {sourcemaps: true})
-    .pipe(
-      // sassPackageImporter handles @import "~bootstrap"
-      // https://github.com/maoberlehner/node-sass-magic-importer/tree/master/packages/node-sass-package-importer
-      CLIOptions.hasFlag('watch') ?
-        sass.sync({quietDeps: true, importer: sassPackageImporter()}).on('error', sass.logError) :
-        sass.sync({quietDeps: true, importer: sassPackageImporter()})
-    )
-    .pipe(postcss([
-      autoprefixer(),
-      // Inline images and fonts
-      postcssUrl({url: 'inline', encodeType: 'base64'}),
-      cssnano()
-    ]))
-    .pipe(build.bundle());
+const sourcemaps = require( 'gulp-sourcemaps' );
+
+export default function processCSS()
+{
+	return gulp.src( project.cssProcessor.source )
+			   .pipe( sourcemaps.init() )
+			   .pipe(
+				   // sassPackageImporter handles @import "~bootstrap"
+				   // https://github.com/maoberlehner/node-sass-magic-importer/tree/master/packages/node-sass-package-importer
+				   CLIOptions.hasFlag( 'watch' ) ?
+					   sass.sync( {
+						   quietDeps : true, importer : sassPackageImporter()
+					   } ).on( 'error', sass.logError ) :
+					   sass.sync( { quietDeps : true, importer : sassPackageImporter() } )
+			   )
+			   .pipe( postcss( [
+				   autoprefixer(),
+				   // Inline images and fonts
+				   postcssUrl( { url : 'inline', encodeType : 'base64' } ),
+				   cssnano()
+			   ] ) )
+			   .pipe(sourcemaps.write())
+			   .pipe( build.bundle() );
 }
 
