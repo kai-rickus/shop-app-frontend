041c4622d434956ffe09f574f654df4f2dde7df5
kai-rickus <kai.rickus@outlook.com>
2022-06-09 20:00:08 +0200

    add shopping cart sub views

--- src/views/shopping-cart-view/shopping-cart-view.ts
+++ src/views/shopping-cart-view/shopping-cart-view.ts
@@ -1,3 +1,46 @@
+import { RouteConfig, Router, RouterConfiguration } from "aurelia-router";
+
+const ROUTES: RouteConfig[] = [
+	{
+		route    : [ "", 'items' ],
+		name     : 'items',
+		moduleId : './views/items-view/items-view',
+		nav      : true,
+		title    : 'Waren'
+	},
+	{
+		route    : [ 'address' ],
+		name     : 'address',
+		moduleId : './views/address-view/address-view',
+		nav      : true,
+		title    : 'Lieferadresse'
+	},
+	{
+		route    : [ 'payment' ],
+		name     : 'payment',
+		moduleId : './views/payment-view/payment-view',
+		nav      : true,
+		title    : 'Zahlungart'
+	},
+	{
+		route    : [ 'overview' ],
+		name     : 'overview',
+		moduleId : './views/overview-view/overview-view',
+		nav      : true,
+		title    : 'Endübersicht'
+	}
+
+];
+
+
 export class ShoppingCartView {
+	router;
+
+	public configureRouter( config: RouterConfiguration, router: Router ): Promise<void> | PromiseLike<void> | void
+	{
+		config.map( ROUTES );
+
+		this.router = router;
+	}
 
 }
