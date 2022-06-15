fad9b1f65fca0bf70824bbee2fec1f0e00fcca77
kai-rickus <kai.rickus@outlook.com>
2022-06-15 21:11:43 +0200

    add Backend connection in shopping cart items view

--- src/views/shopping-cart-view/views/items-view/items-view.ts
+++ src/views/shopping-cart-view/views/items-view/items-view.ts
@@ -1,3 +1,30 @@
-export class ItemsView {
+import { autoinject, bindable } from "aurelia-framework";
+import environment              from "../../../../environment";
+import { ShopUser }             from "../../../../services/shop-user";
 
+@autoinject()
+export class ItemsView
+{
+	user: ShopUser
+	items = [];
+
+	constructor( user: ShopUser )
+	{
+		this.user = user;
+	}
+
+	async attached()
+	{
+		this.items = await this.getCardItems()
+		debugger
+	}
+
+	async getCardItems()
+	{
+		const response = await fetch( `${environment.backendBaseUrl}cart/get`, {
+			headers : { "Authorization" : "Bearer " + this.user.accessToken }
+		} );
+		const data     = await response.json()
+		return data.items
+	}
 }
