3e95fa13c7c9129435f901a565e240ada3a2394b
kai-rickus <kai.rickus@outlook.com>
2022-08-24 19:41:52 +0200

    Refactor theme variable into own variables file

--- aurelia_project/generators/component.ts
+++ aurelia_project/generators/component.ts
@@ -1,36 +1,41 @@
-import { inject } from 'aurelia-dependency-injection';
+import { inject }                               from 'aurelia-dependency-injection';
 import { Project, ProjectItem, CLIOptions, UI } from 'aurelia-cli';
-import * as path from 'path';
+import * as path                                from 'path';
 
-@inject(Project, CLIOptions, UI)
-export default class ElementGenerator {
-  constructor(private project: Project, private options: CLIOptions, private ui: UI) { }
+@inject( Project, CLIOptions, UI )
+export default class ElementGenerator
+{
+	constructor( private project: Project, private options: CLIOptions, private ui: UI ){ }
 
-  async execute() {
-    const name = await this.ui.ensureAnswer(
-      this.options.args[0],
-      'What would you like to call the component?'
-    );
+	async execute()
+	{
+		const name = await this.ui.ensureAnswer(
+			this.options.args[ 0 ],
+			'What would you like to call the component?'
+		);
 
-    const subFolders = await this.ui.ensureAnswer(
-      this.options.args[1],
-      'What sub-folder would you like to add it to?\nIf it doesn\'t exist it will be created for you.\n\nDefault folder is "." relative to the source folder src/', "."
-    );
+		const subFolders = await this.ui.ensureAnswer(
+			this.options.args[ 1 ],
+			'What sub-folder would you like to add it to?\nIf it doesn\'t exist it will be created for you.\n\nDefault folder is "." relative to the source folder src/', "."
+		);
 
-    let fileName = this.project.makeFileName(name);
-    let className = this.project.makeClassName(name);
+		let fileName  = this.project.makeFileName( name );
+		let className = this.project.makeClassName( name );
 
-    this.project.root.add(
-      ProjectItem.text(path.join(subFolders, fileName + '.ts'), this.generateJSSource(className)),
-      ProjectItem.text(path.join(subFolders, fileName + '.html'), this.generateHTMLSource(className))
-    );
+		this.project.root.add(
+			ProjectItem.text( path.join( subFolders, fileName + '.ts' ), this.generateJSSource( className ) ),
+			ProjectItem.text( path.join( subFolders, fileName + '.html' ), this.generateHTMLSource( fileName ) ),
+		ProjectItem.text( path.join( subFolders, fileName + '.scss' ), this.generateCSSSource( fileName ) )
+	)
+		;
 
-    await this.project.commitChanges();
-    await this.ui.log(`Created ${name} in the '${path.join(this.project.root.name, subFolders)}' folder`);
-  }
+		await this.project.commitChanges();
+		await this.ui.log( `Created ${name} in the '${path.join( this.project.root.name, subFolders )}' folder` );
+	}
 
-  generateJSSource(className) {
-    return `export class ${className} {
+	generateJSSource( className )
+	{
+		return `export class ${className} {
   message: string;
 
   constructor() {
@@ -38,12 +43,20 @@ export default class ElementGenerator {
   }
 }
 `
-  }
+	}
 
-  generateHTMLSource(className) {
-    return `<template>
-  <h1>\${message}</h1>
+	generateCSSSource( fileName )
+	{
+		return`.${fileName}{\n
+	}	
+`
+	}
+
+	generateHTMLSource( fileName )
+	{
+		return `<template class="${fileName}">
+	<require from="./${fileName}.css"></require>
 </template>
 `
-  }
+	}
 }
--- src/views/shopping-cart-view/views/address-view/address-view.ts
+++ src/views/shopping-cart-view/views/address-view/address-view.ts
@@ -1,3 +1,5 @@
+import { MdcRadio } from "@aurelia-mdc-web/radio";
+
 export class AddressView
 {
 	addresses = [
