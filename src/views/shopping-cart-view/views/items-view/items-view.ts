ffab3e17b500e9be661917f2ccaa1e9a115033ae
kai-rickus <kai.rickus@outlook.com>
2022-11-18 17:18:55 +0100

    add shopping cart amount select logic

--- src/views/shopping-cart-view/views/items-view/items-view.ts
+++ src/views/shopping-cart-view/views/items-view/items-view.ts
@@ -5,28 +5,28 @@ import environment             from "../../../../environment";
 import { SIGNAL_CART_UPDATED } from "../../../../resources/components/product-details-main/product-details-main";
 import { ShopUser }            from "../../../../services/shop-user";
 import { ShoppingCartView }    from "../../shopping-cart-view";
-import { MDCSelect }           from '@material/select';
 
 @autoinject()
 export class ItemsView
 {
 	user: ShopUser
-	select = new MDCSelect( document.querySelector( '.mdc-select' ) );
+	value;
 
 	constructor( user: ShopUser, private _signaler: BindingSignaler, private _shoppingCart: ShoppingCartView, private _taskqueue: TaskQueue )
 	{
 		this.user = user;
-
-		this.select.listen( 'MDCSelect:change', () =>
-		{
-			alert( `Selected option at index ${this.select.selectedIndex} with value "${this.select.value}"` );
-		} );
 	}
 
 	attached()
 	{
 		this._shoppingCart.setHeightAfterRouting()
+
 	}
+
+	// onSelectionChange()
+	// {
+	// 	console.log( event );
+	// }
 
 	async getCardItems()
 	{
@@ -45,6 +45,7 @@ export class ItemsView
 
 	async setCartItems( id, amount )
 	{
+		debugger
 		const response = await fetch( `${environment.backendBaseUrl}cart/set/${id}/${amount}`, {
 			method  : "put",
 			headers : { "Authorization" : "Bearer " + this.user.accessToken }
